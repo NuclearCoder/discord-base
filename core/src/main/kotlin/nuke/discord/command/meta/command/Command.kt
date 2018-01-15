63e80e6e65cdf77ef212fd63cbd2f0adc3df8481
NuclearCoder <hd85.ms@gmail.com>
2018-01-15 15:14:57 +0100

    Added lambda constructor to the command class

--- core/src/main/kotlin/nuke/discord/command/meta/command/Command.kt
+++ core/src/main/kotlin/nuke/discord/command/meta/command/Command.kt
@@ -5,6 +5,14 @@ import nuke.discord.util.discord.hasSufficientPermissions
 
 abstract class Command(val requiredPermission: PermLevel = PermLevel.USER) {
 
+    companion object {
+        inline operator fun invoke(perm: PermLevel = PermLevel.USER,
+                                   crossinline block: Command.(CommandContext) -> Unit)
+                = object : Command(perm) {
+                    override fun onInvoke(context: CommandContext) = block(context)
+                }
+    }
+
     abstract fun onInvoke(context: CommandContext)
 
     protected fun CommandContext.hasSufficientPermission(permission: PermLevel) =
