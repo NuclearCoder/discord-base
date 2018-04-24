a3c0d3b01058f51a157858be3fe2fb44f9692c63
Chloé Ichinose <chloedv99@gmail.com>
2018-04-24 14:46:11 +0200

    Added shorthand properties to CommandContext

--- core/src/main/kotlin/nuke/discord/command/meta/CommandContext.kt
+++ core/src/main/kotlin/nuke/discord/command/meta/CommandContext.kt
@@ -1,9 +1,9 @@
 package nuke.discord.command.meta
 
 import com.thatsnomoon.kda.extensions.sendAsync
+import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.MessageBuilder
-import net.dv8tion.jda.core.entities.Message
-import net.dv8tion.jda.core.entities.User
+import net.dv8tion.jda.core.entities.*
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import nuke.discord.bot.NukeBot
 import nuke.discord.util.discord.MessageTokenizer
@@ -31,5 +31,19 @@ class CommandContext(val event: MessageReceivedEvent,
         append("missing arguments: ")
         append(details)
     }
+
+    val jda get(): JDA = message.jda
+
+    val author get(): User = message.author
+    val member get(): Member = message.member
+
+    val guild get(): Guild = message.guild
+    val category get(): Category = message.category
+    val channel get(): MessageChannel = message.channel
+
+    val mentionedChannels get(): List<MessageChannel> = message.mentionedChannels
+    val mentionedMembers get(): List<Member> = message.mentionedMembers
+    val mentionedRoles get(): List<Role> = message.mentionedRoles
+    val mentionedUsers get(): List<User> = message.mentionedUsers
 
 }
